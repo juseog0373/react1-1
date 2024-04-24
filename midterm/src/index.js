dee54ae2163734fee3831c4fd70c793f8672fc2d
jskim <juseog0373@naver.com>
2024-04-24 11:33:25 +0900

    Q2 commit

--- midterm/src/index.js
+++ midterm/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Midterm from './midterm/Midterm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Midterm />
   </React.StrictMode>
 );
 
