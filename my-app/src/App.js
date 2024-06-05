c34f0d80d7c5f92321441637876137632fbfdfd6
jskim <juseog0373@naver.com>
2024-06-05 12:48:37 +0900

    week14 커밋

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,12 +8,16 @@ import LandingPage from './LandingPage';
 import NumberList from './NumberList';
 import AttendanceBook from './AttendanceBook';
 import SignUp from './chapter11/SignUp';
+import Calculator from './chapter12/Calculator';
+import WelcomeDialog from './chapter13/WelcomeDialog';
+import SplitPane from './chapter13/SplitPane';
+
 
 
 function App() {
   return (
     <div className="App">
-      <SignUp/>
+      <SplitPane left={<Cotacts/>} right={<Chat/>}/>
     </div>
   );
 }
